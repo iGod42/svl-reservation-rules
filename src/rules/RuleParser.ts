c305cc0bdfa25d37c0b074e7100e3e60d3711cea
Robert Spari <robert.spari@gmail.com>
2020-04-24 17:56:14 +0200

    resolved circular depenency

--- src/rules/RuleParser.ts
+++ src/rules/RuleParser.ts
@@ -1,5 +1,26 @@
-import { parsers } from "."
+import limitHours from "./limitHours"
+import limitToCourts from "./limitToCourts"
+import maxPerRange from "./maxPerRange"
+import noDoubleCourtReservations from "./noDoubleCourtReservations"
+import noEntryAfterWeek from "./noEntryAfterWeek"
+import noEntryBefore from "./noEntryBefore"
+import noEntryPastHour from "./noEntryPastHour"
+import noOhterReservations from "./noOtherReservations"
+import userIsInIt from "./userIsInIt"
+import { RuleParser } from "./api"
 import { RuleDefinition, RuleEvaluation } from "./api"
+
+export const parsers: RuleParser[] = [
+	limitHours,
+	limitToCourts,
+	maxPerRange,
+	noDoubleCourtReservations,
+	noEntryAfterWeek,
+	noEntryBefore,
+	noEntryPastHour,
+	noOhterReservations,
+	userIsInIt
+]
 
 export const parseRule = (ruleDefinition: RuleDefinition): RuleEvaluation => {
 	const re = parsers.map(parser => parser(ruleDefinition)).find(re => !!re)
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,26 +1,3 @@
-import limitHours from "./limitHours"
-import limitToCourts from "./limitToCourts"
-import maxPerRange from "./maxPerRange"
-import noDoubleCourtReservations from "./noDoubleCourtReservations"
-import noEntryAfterWeek from "./noEntryAfterWeek"
-import noEntryBefore from "./noEntryBefore"
-import noEntryPastHour from "./noEntryPastHour"
-import noOhterReservations from "./noOtherReservations"
-import userIsInIt from "./userIsInIt"
-import { RuleParser } from "./api"
-
-export const parsers: RuleParser[] = [
-	limitHours,
-	limitToCourts,
-	maxPerRange,
-	noDoubleCourtReservations,
-	noEntryAfterWeek,
-	noEntryBefore,
-	noEntryPastHour,
-	noOhterReservations,
-	userIsInIt
-]
-
-export { parseRule, parseRules } from "./RuleParser"
+export { parsers, parseRule, parseRules } from "./RuleParser"
 
 export { RuleDefinition } from "./api"
