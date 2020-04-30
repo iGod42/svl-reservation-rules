118e3911c7f1c6f28a3529621f1e69c792e2c40c
Robert Spari <robert.spari@gmail.com>
2020-04-30 17:08:06 +0200

    lint fix

--- src/rules/noEntryPastHour.ts
+++ src/rules/noEntryPastHour.ts
@@ -6,7 +6,7 @@ import {
 	BulkRuleEvaluationOptions
 } from "./api"
 import { NoEntryPastHourDefinition } from "./api/NoEntryPastHourDefinition"
-import formatWeekdays from '../tools/formatWeekdays'
+import formatWeekdays from "../tools/formatWeekdays"
 
 const allWeekDays = [0, 1, 2, 3, 4, 5, 6]
 class NoEntryPastHour implements Rule {
@@ -17,10 +17,13 @@ class NoEntryPastHour implements Rule {
 		this.definition = { weekDays: allWeekDays, ...definition }
 	}
 
-	private message = () => (this.definition.weekDays !== allWeekDays ? formatWeekdays(this.definition.weekDays, {
-		weekdayText: "Wochentags",
-		weekendText: "Wochenends"
-	}) + " k" : "K") + `eine Reservierungen ab ${this.definition.hour} Uhr erlaubt`
+	private message = () =>
+		(this.definition.weekDays !== allWeekDays
+			? formatWeekdays(this.definition.weekDays, {
+					weekdayText: "Wochentags",
+					weekendText: "Wochenends"
+			  }) + " k"
+			: "K") + `eine Reservierungen ab ${this.definition.hour} Uhr erlaubt`
 
 	private filter = (compDate: Date) =>
 		compDate.getHours() >= this.definition.hour &&
