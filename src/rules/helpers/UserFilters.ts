7da7dcb1e5f62e1170a2ca1131ed0f06a20591dc
Robert Spari <robertspari@Roberts-MacBook-Pro.local>
2020-05-26 13:24:37 +0200

    Added hacky fix for guest users

--- src/rules/helpers/UserFilters.ts
+++ src/rules/helpers/UserFilters.ts
@@ -1,11 +1,18 @@
 import { Reservation } from "../api"
 
+const ignoreIds = ["gast", "g", "X_Gast"]
+
 export const findFirstMatchingUser = (
 	reservation: Reservation,
 	userIds: string[]
-) =>
-	userIds.find(id => reservation.reservedBy.id === id) ||
-	userIds.find(id => reservation.players.find(player => player.id === id))
+) => {
+	//hack assuming last one is the one reserving
+	const filterUids = userIds.slice(0, -1)
+		.filter(uid => !ignoreIds.includes(uid))
+		.concat(userIds.slice(-1))
+	return filterUids.find(id => reservation.reservedBy.id === id) ||
+		filterUids.find(id => reservation.players.find(player => player.id === id))
+}
 
 export const getFilterForSameUser = (bRes: Reservation) => (
 	aRes: Reservation
