984b7fed6083b98307d5776274809d73cd4ef297
Robert Spari <robert.spari@gmail.com>
2021-05-16 10:47:44 +0200

    beginning of week now strips time component

--- src/tools/getBeginningOfWeek.ts
+++ src/tools/getBeginningOfWeek.ts
@@ -1,5 +1,7 @@
+import { trimTimeComponent } from "./trimTimeComponent"
+
 export const getBeginningOfWeek = (date: Date = new Date()): Date => {
-	const d = new Date(date)
+	const d = trimTimeComponent(date)
 	const day = d.getDay(),
 		diff = d.getDate() - day + (day === 0 ? -6 : 1) // adjust when day is sunday
 	return new Date(d.setDate(diff))
